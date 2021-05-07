25595e915eca3778ba3296184745dd4f2205b379
nand42 <dario.chinelli95@gmail.com>
2021-05-07 12:18:14 +0200

    first IQ test

---
+++ test.py
@@ -0,0 +1,42 @@
+### Test di minima comprensione matematica/informatica
+### Test di compilazione minima di un file python
+
+import random
+import time
+import os
+
+clear = lambda: os.system("clear")
+
+clear()
+
+print("... ... ... ... ... ... ... ... .. .. .. .. .. .. . . . . . . .")
+print("Hello!")
+print("Rispondi al seguente quesito per sapere chi sei \n")
+print("... ... ... ... ... ... ... ... .. .. .. .. .. .. . . . . . . .")
+
+a = random.randrange(0,100,1)
+b = a
+while a == b:
+	b = random.randrange(0,100,1)
+
+n = str( a + b )
+
+time.sleep(1)
+
+r = str(input( str(a) + " + " + str(b) + " = "))
+
+time.sleep(1)
+print("\n... ... ... ... ... ... ... ... .. .. .. .. .. .. . . . . . . .")
+time.sleep(1)
+
+if ( r == n ):
+	print("------------------> Bene, sei una persona in gamba.")
+else:
+	print("------------------> Sei una capra.")
+	print("Soluzione: " + str(a) + " + " + str(b) + " = " + n)
+
+print("\n... ... ... ... ... ... ... ... .. .. .. .. .. .. . . . . . . .")
+
+commento = input("lascia un commento: ")
+
+clear()
