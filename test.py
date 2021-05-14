e36f122f3f140c2d38a24834327bcce64cd4b538
nand42 <dario.chinelli95@gmail.com>
2021-05-14 08:19:18 +0200

    now it can write the comment on file

--- test.py
+++ test.py
@@ -4,8 +4,14 @@
 import random
 import time
 import os
+import json
 
 clear = lambda: os.system("clear")
+
+
+def writefile(list, filename="test_risposte.txt"):
+	with open(filename, 'w') as filehandle:
+		json.dump(list, filehandle)
 
 start = "via"
 
@@ -13,6 +19,8 @@ while start != "exit":
 
 	clear()
 
+	list_risp = []
+	
 	print("... ... ... ... ... ... ... ... .. .. .. .. .. .. . . . . . . .")
 	print("Hello!")
 	start = input("Premi INVIO per iniziare")
@@ -28,25 +36,46 @@ while start != "exit":
 
 	n = str( a + b )
 
+	addizione = str(a) + " + " + str(b) + " = "
+	
+	list_risp.append("addizione: " + addizione + str(n))
+	
 	time.sleep(1)
 
-	r = str(input( str(a) + " + " + str(b) + " = "))
+	risp = str(input( addizione ))
+
+	list_risp.append("risposta: " + risp)
 	
+	if risp == "exit":
+		writefile(list_risp)
+		break
+        	
 	time.sleep(1)
 	print("\n... ... ... ... ... ... ... ... .. .. .. .. .. .. . . . . . . .")
 	time.sleep(1)
 
-	if ( r == n ):
+	if ( risp == n ):
+		list_risp.append("gauss")
 		print("------------------> Bene, sei una persona in gamba.")
 	else:
+		list_risp.append("capra")
 		print("------------------> Sei una capra.")
 		print("Soluzione: " + str(a) + " + " + str(b) + " = " + n)
 
 	print("\n... ... ... ... ... ... ... ... .. .. .. .. .. .. . . . . . . .")
 
 	commento = input("lascia un commento: ")
+	list_risp.append("commento: " + commento)
 
+	writefile(list_risp)
+	
 	if commento == "exit":
 		break
 	
 	clear()
+
+print("Grazie per aver giocato, presto nuove domande...")
+
+
+
+
